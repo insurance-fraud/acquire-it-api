a28800de16e1ec9c297536bf184abd5909fe4cb5
nikolalsvk <nikolaseap@gmail.com>
2018-04-23 18:59:13 +0200

    Log to standard output

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module AcquireItApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Log to standarad out
+    config.logger = Logger.new(STDOUT)
   end
 end
